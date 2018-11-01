a713d530815a4dce4ec408730942bf7c4c14622d
Sherry Martin <sm674@cornell.edu>
2018-11-01 12:02:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,21 @@
+require_relative 'config/environment'
 require 'sinatra/base'
 require 'bundler'
 Bundler.require
-    
+
 class App < Sinatra::Base
     get '/' do
       erb :index
     end
-  
+
+    get '/:newteam' do
+      erb params[:newteam].to_sym
+    end
+
     get '/:form_type' do
       erb params[:form_type].to_sym
     end
-  
+
     post '/:form_type' do
       erb :results
     end
